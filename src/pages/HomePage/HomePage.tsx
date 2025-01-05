1a5682c37711922cf9bee4d1e36fde8e5cb2a09c
Kamila-Lohvynenko <kamila.lohvynenko@gmail.com>
2025-01-06 00:53:06 +0100

    add photo and English text to hero section

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,12 +1,30 @@
 import ChangeLanguageBtn from "../../components/ChangeLanguageBtn/ChangeLanguageBtn";
 import styles from "./HomePage.module.scss";
+import myPhoto from "../../images/my_photo.jpeg";
+
+import { useTranslation } from "react-i18next";
 
 const HomePage = () => {
+  const { t } = useTranslation();
+
   return (
     <div className={styles.page}>
       <ChangeLanguageBtn />
-      <div className={styles.content}></div>
-      <div className={styles.im}></div>
+      <div className={styles.pageWrapper}>
+        <div className={styles.content}>
+          <h2 className={styles.subtitle}> {t("heroSection.subtitle")}</h2>
+          <h1 className={styles.title}>
+            <span className={styles.accent}>
+              {t("heroSection.titlePart1")}{" "}
+            </span>
+            {t("heroSection.titlePart2")}
+          </h1>
+          <p>{t("heroSection.text")}</p>
+        </div>
+        <div className={styles.imageWrapper}>
+          <img className={styles.image} src={myPhoto} alt="my photo" />
+        </div>
+      </div>
     </div>
   );
 };
